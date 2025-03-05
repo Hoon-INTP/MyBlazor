ab2e90b43388067dee3ed91796d3a805ae442fba
Hoon-INTP <146914698+Hoon-INTP@users.noreply.github.com>
2025-03-05 11:59:15 +0900

    Converting to Fluent UI
    1. MainLayout
    2. NavMenu

--- MyHdfViewer/Program.cs
+++ MyHdfViewer/Program.cs
@@ -4,16 +4,16 @@ using Microsoft.AspNetCore.Components.Forms;
 using MyHdfViewer;
 using MyHdfViewer.Services;
 //using MudBlazor.Services;
-//using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddMudServices();
-//builder.Services.AddFluentUIComponents();
-//builder.Services.AddDataGridEntityFrameworkAdapter();
 //builder.Services.AddHttpClient();
+builder.Services.AddFluentUIComponents();
+//builder.Services.AddDataGridEntityFrameworkAdapter();
 builder.Services.AddScoped<IHdf5FileReader, Hdf5FileReader>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
