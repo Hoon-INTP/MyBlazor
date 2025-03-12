c2c4fd1db3779ad3d1dd398decd5bf30c99de4f8
Hoon-INTP <146914698+Hoon-INTP@users.noreply.github.com>
2025-03-12 09:08:31 +0900

    Chart 추가 Smaple file 추가

--- MyHdfViewer/Program.cs
+++ MyHdfViewer/Program.cs
@@ -3,16 +3,18 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.Forms;
 using MyHdfViewer;
 using MyHdfViewer.Services;
-//using MudBlazor.Services;
+using MudBlazor.Services;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddMudServices();
 //builder.Services.AddHttpClient();
 builder.Services.AddFluentUIComponents();
+builder.Services.AddMudServices(config => {
+    config.SnackbarConfiguration.PositionClass = MudBlazor.Defaults.Classes.Position.TopRight;
+});
 //builder.Services.AddDataGridEntityFrameworkAdapter();
 builder.Services.AddScoped<IHdf5FileReader, Hdf5FileReader>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
